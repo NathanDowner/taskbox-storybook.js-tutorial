1224565471116ffb7d03e2cfe2b9b6cd846fa4d0
Nathan Downer <nathandowner123@gmail.com>
2022-01-08 19:23:40 -0500

    add demo tag line to title

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -20,7 +20,7 @@ export function PureInboxScreen({ error }) {
     <div className="page lists-show">
       <nav>
         <h1 className="title-page">
-          <span className="title-wrapper">Taskbox</span>
+          <span className="title-wrapper">Taskbox (Story book demo)</span>
         </h1>
       </nav>
       <TasksContainer />
